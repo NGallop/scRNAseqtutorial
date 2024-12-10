160363315e2026b0d4cfc5ba680548234ce96159
Niall Gallop <62139335+NGallop@users.noreply.github.com>
2024-12-10 14:52:04 +0000

    plots from scanpy saved to file, stored in plots

--- QC/data_investigation.py
+++ QC/data_investigation.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-sc.settings.figdir = "plots/"
+sc.settings.figdir = "plots/data_investigation/"
 
 # read in raw data
 raw_data = sc.read_h5ad('data/Hufallopiantube.h5ad')
@@ -12,13 +12,14 @@ raw_data = sc.read_h5ad('data/Hufallopiantube.h5ad')
 # create the layers dictionary to store alternative representations of X where "counts" is the key
 raw_data.layers["counts"] = raw_data.X.copy()
 
-
+print("Plot display suppressed; plots saved directly to file.")
+print("\n\n")
 # visualise the data in the terminal
 print("Visualising the Observations in the AnnData object:")
 print(raw_data.obs)
 print("\n\n")
-print("Highest Expressed Genes plot available. Please close to continue...")
-#sc.pl.highest_expr_genes(raw_data, n_top=5)
+print("Highest Expressed Genes plot available.")
+sc.pl.highest_expr_genes(raw_data, n_top=5, save='_highest_exp_genes.png', show=False)
 print("\n\n")
 
 ####### Clean Data #######
@@ -33,16 +34,16 @@ sc.pp.filter_genes(raw_data, min_cells= 3)
 sc.pp.calculate_qc_metrics(raw_data, percent_top=[50/100/200/500], inplace=True, log1p=False, )
 
 # Use the above calculated QC metrics and visualise in a violin plot
-print("Quality Control Metrics available. Please close to continue...")
-#sc.pl.violin(raw_data, ['n_genes_by_counts','total_counts'], jitter=0.4, multi_panel=True)
+print("Quality Control Metrics available.")
+sc.pl.violin(raw_data, ['n_genes_by_counts','total_counts'], jitter=0.4, multi_panel=True, save='_raw_qc.png', show=False)
 print("\n\n")
 
 # Filter again based on this to remove n_gene_by_counts of <6000 and 'total_counts' <1500000
 raw_data = raw_data[raw_data.obs.n_genes_by_counts <6000,:]
 data = raw_data[raw_data.obs.total_counts <1500000,:]
 # Visualise again
-print("Filtered Quality Control Metrics available. Please close to continue...")
-#sc.pl.violin(data, ['n_genes_by_counts','total_counts'], jitter=0.4, multi_panel=True)
+print("Filtered Quality Control Metrics available.")
+sc.pl.violin(data, ['n_genes_by_counts','total_counts'], jitter=0.4, multi_panel=True, save='_sliced_qc.png', show=False)
 print("\n\n")
 
 
@@ -67,22 +68,22 @@ sc.pp.log1p(data)
 
 # 2. Crop to highly variable genes
 sc.pp.highly_variable_genes(data, min_mean=0.0125, max_mean=3, min_disp=0.5)
-sc.pl.highly_variable_genes(data)
+sc.pl.highly_variable_genes(data, save='_high_variable_genes.png', show=False)
 data.raw = data
 data = data [:, data.var.highly_variable]
 sc.pp.scale(data, max_value=10)
 
 # 3. PCA
 sc.tl.pca(data, svd_solver='arpack')
-sc.pl.pca_variance_ratio(data, log=True)
-sc.pl.pca(data, color=["Disease_stage"])
+sc.pl.pca_variance_ratio(data, log=True, save="_variance_ratio.png", show=False)
+sc.pl.pca(data, color=["Disease_stage"], save='_diseas_stage.png', show=False)
 
 # 4. Nearest Neighbour
-sc.pp.neighbors(data, n_pcs=10)
+sc.pp.neighbors(data, n_pcs=8)
 
 # 5. UMAP
 sc.tl.umap(data)
-sc.pl.umap(data, color=["Disease_stage"])
+sc.pl.umap(data, color=["Disease_stage"], save="_umap_disease_stage.png", show=False)
 
 
 
@@ -98,11 +99,11 @@ Cluster the cells to identify similar groups of cells
 
 # 1. Perform leiden clustering, then visualise clusters on the umap
 sc.tl.leiden(data, resolution=0.2)
-sc.pl.umap(data, color=['leiden'])
+sc.pl.umap(data, color=['leiden'], save="_leiden_umap.png", show=False)
 
 # 2. Calculate the top differentially expressed genes in each leiden cluster
 sc.tl.rank_genes_groups(data,'leiden', method='wilcoxon')
-sc.pl.rank_genes_groups(data, n_genes=25)
+sc.pl.rank_genes_groups(data, n_genes=25, save="_ranked_genes.png", show=False)
 
 # 3. Export the ranked genes with scores to a csv file
 Export_csv = False
@@ -115,16 +116,34 @@ if Export_csv:
     celltypemarkergenes.to_csv('data/celltypemarkergenes_export.csv', index=False)
 
 # 4. Plot the top 20 differentially expressed genes in a heatmap
-sc.pl.rank_genes_groups_heatmap(data, n_genes=20, swap_axes=True, show_gene_labels=False, vmin=-3, vmax=3 )
+sc.pl.rank_genes_groups_heatmap(data, n_genes=20, swap_axes=True, show_gene_labels=False, vmin=-3, vmax=3, save="_ranked_genes_groups.png", show=False)
 
 # 5. Visualise marker genes to determine the cell types present in each cluster
-gene_list = ["PAX8", "SOX17", "OVGP1", "MUC16", 
- #"FOXJ1", "CCDC78", "PIFO", 
- #"PTPRC", "CD3D", "CD14", 
- #"COL1A1", "COL3A1", "DCN", 
- "KRT17", "WFDC2"]
+sec_epi_list = ["PAX8", "SOX17", "OVGP1", "MUC16"]
+cil_epi_list = ["FOXJ1", "CCDC78", "PIFO"]
+immune_list = ["PTPRC", "CD3D", "CD14"]
+fibroblast_list = ["COL1A1", "COL3A1", "DCN"]
+stic_list = ["KRT17", "WFDC2"]
+
+gene_lists = [sec_epi_list, cil_epi_list, immune_list, fibroblast_list, stic_list]
+
 print("Final violin plot to decide dictionary. Close to close program...")
-sc.pl.violin(data, gene_list, groupby = 'leiden', save='_gene_exp.png')
+iteration = 0
+for gene_list in gene_lists:
+    iteration += 1
+    if gene_list[0] == "PAX8":
+        file_namer = "secretory_epithelial"
+    elif gene_list[0] == "FOXJ1":
+        file_namer = "ciliated_epithelial"
+    elif gene_list[0] == "PTPRC":
+        file_namer = "immune_cell"
+    elif gene_list[0] == "COL1A1":
+        file_namer = "fibroblast"
+    elif gene_list[0] == "KRT17":
+        file_namer = "STIC"
+    else:
+        file_namer = f"unknown{iteration}"
+    sc.pl.violin(data, gene_list, groupby = 'leiden', save=f'_gene_exp_{file_namer}.png', show=False)
 
 
 # 6. Create a dictionary defining leiden clusters and their respective cell types
@@ -133,8 +152,8 @@ celltypedict = {
         '1' : 'Secretory Epithelial-2',
         '2' : 'Ciliated Epithelial',
         '3' : 'STIC-1',
-        '4' : 'STIC-2',
-        '5' : 'Immune',
+        '4' : 'Immune',
+        '5' : 'STIC-2',
         '6' : 'Fibroblast'
         }
 
@@ -142,4 +161,4 @@ data.obs['Celltype'] = data.obs['leiden'].map(celltypedict)
 
 # 7. Plot a UMPA coloured by leiden cell type clusters
 sc.tl.umap(data)
-sc.pl.umap(data, color=["Celltype"])
+sc.pl.umap(data, color=["Celltype"], save="_cell_type_umap.png", show=False)
